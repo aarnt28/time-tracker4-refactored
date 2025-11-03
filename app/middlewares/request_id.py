f88396da942dab9f4552f28795df964545d3e079
aturner67 <43986439+aarnt28@users.noreply.github.com>
2025-11-02 20:27:22 -0600

    Revert "fix: isolate request logging from uvicorn access logger"

--- app/middlewares/request_id.py
+++ app/middlewares/request_id.py
@@ -11,7 +11,7 @@ from starlette.responses import Response
 
 request_id_ctx_var: ContextVar[str | None] = ContextVar("request_id", default=None)
 principal_ctx_var: ContextVar[str | None] = ContextVar("principal_id", default=None)
-logger = logging.getLogger("app.request")
+logger = logging.getLogger("uvicorn.access")
 
 
 class RequestIdMiddleware(BaseHTTPMiddleware):
